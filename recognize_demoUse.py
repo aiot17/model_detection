17c8a17d140e0b1751af923f4febc4d537bd3cde
kevinchang <kevin@hotmail.com>
2022-07-06 19:09:06 +0800

    first commit

---
+++ recognize_demoUse.py
@@ -0,0 +1,180 @@
+# USAGE
+# python recognize.py --detector face_detection_model \
+#    --embedding-model openface_nn4.small2.v1.t7 \
+#    --recognizer output/recognizer.pickle \
+#    --le output/le.pickle --image images/adrian.jpg
+
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import pickle
+import cv2
+import os
+import socket
+import socketio
+
+
+# ****this py file is revised for demo purpose****
+
+# below java and nodejs socket
+def yes(name):
+    msg = {"azen":"001","Chao":"002","Dav":"003","hubert_test":"004","kk":"005","TzuYao":"006"}.get(name)
+    HOST = '192.168.22.196'
+    PORT = 5000
+    print(msg)
+    mainSocket(HOST,PORT,msg)
+
+def no(msg):
+    HOST = '192.168.22.'
+    PORT = 5550
+    print(msg)
+    mainSocket(HOST,PORT,msg)
+
+def mainSocket(HOST,PORT,msg):
+    sio = socketio.Client()
+
+    @sio.event
+    def connect():
+        print('connection established')
+        sio.emit('dataPy', "0")
+
+    # @sio.event
+    # def my_message(data):
+    #     print('message received with ', data)
+    #     # sio.emit('dataPy', "abc")
+        
+    @sio.event
+    def disconnect():
+        print('disconnected from server')
+
+    sio.connect('http://192.168.22.79:5450')
+    sio.wait()
+# above java and nodejs socket
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=False,default='./images/00000.png',
+    help="path to input image")
+ap.add_argument("-d", "--detector", required=False,default='face_detection_model',
+    help="path to OpenCV's deep learning face detector")
+ap.add_argument("-m", "--embedding-model", required=False,default='openface_nn4.small2.v1.t7',
+    help="path to OpenCV's deep learning face embedding model")
+ap.add_argument("-r", "--recognizer", required=False,default='output/recognizer.pickle',
+    help="path to model trained to recognize faces")
+ap.add_argument("-l", "--le", required=False,default='output/le.pickle',
+    help="path to label encoder")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+    help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# load our serialized face detector from disk
+print("[INFO] loading face detector...")
+protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
+modelPath = os.path.sep.join([args["detector"],
+    "res10_300x300_ssd_iter_140000_fp16.caffemodel"])
+detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+# load our serialized face embedding model from disk
+print("[INFO] loading face recognizer...")
+embedder = cv2.dnn.readNetFromTorch(args["embedding_model"])
+
+# load the actual face recognition model along with the label encoder
+recognizer = pickle.loads(open(args["recognizer"], "rb").read())
+le = pickle.loads(open(args["le"], "rb").read())
+
+# load the image, resize it to have a width of 600 pixels (while
+# maintaining the aspect ratio), and then grab the image dimensions
+image = cv2.imread(args["image"])
+image = imutils.resize(image, width=600)
+(h, w) = image.shape[:2]
+
+# construct a blob from the image
+imageBlob = cv2.dnn.blobFromImage(
+    cv2.resize(image, (300, 300)), 1.0, (300, 300),
+    (104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+# apply OpenCV's deep learning-based face detector to localize
+# faces in the input image
+detector.setInput(imageBlob)
+detections = detector.forward()
+
+# loop over the detections
+for i in range(0, detections.shape[2]):
+    # extract the confidence (i.e., probability) associated with the
+    # prediction
+    confidence = detections[0, 0, i, 2]
+
+    # filter out weak detections
+    if confidence > args["confidence"]:
+        # compute the (x, y)-coordinates of the bounding box for the
+        # face
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
+
+        # extract the face ROI
+        face = image[startY:endY, startX:endX]
+        (fH, fW) = face.shape[:2]
+
+        # ensure the face width and height are sufficiently large
+        if fW < 20 or fH < 20:
+            continue
+
+        # construct a blob for the face ROI, then pass the blob
+        # through our face embedding model to obtain the 128-d
+        # quantification of the face
+        faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),
+            (0, 0, 0), swapRB=True, crop=False)
+        embedder.setInput(faceBlob)
+        vec = embedder.forward()
+
+        # perform classification to recognize the face
+        preds = recognizer.predict_proba(vec)[0]
+        j = np.argmax(preds)
+        proba = preds[j]
+        name = le.classes_[j]
+
+        # 以下機率與socket
+        # print(f"{name}, {proba*100:.2f} %")
+        if proba > 0.5:
+            msg = name
+            # print(f"this is {msg}")
+            HOST = '192.168.22.196'
+            PORT = 5000
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((HOST, PORT))
+            s.send(msg.encode())
+            s.close()
+        else:
+            # sio = socketio.Client()
+
+            # @sio.event
+            # def connect():
+            #     print('connection established')
+            #     sio.emit('dataPy', "0")
+
+            # @sio.event
+            # def my_message(data):
+            #     print('message received with ', data)
+            #     # sio.emit('dataPy', "abc")
+                
+            # @sio.event
+            # def disconnect():
+            #     print('disconnected from server')
+
+            # sio.connect('http://192.168.22.79:5450')
+            # sio.wait()
+            r = yes(name) if proba > 0.5 else no(0)
+        # 以上機率與socket
+        # draw the bounding box of the face along with the associated
+        # probability
+        # text = "{}: {:.2f}%".format(name, proba * 100)
+        # y = startY - 10 if startY - 10 > 10 else startY + 10
+        # cv2.rectangle(image, (startX, startY), (endX, endY),
+        #     (0, 0, 255), 2)
+        # cv2.putText(image, text, (startX, y),
+        #     cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 255), 1)
+
+# show the output image
+# cv2.imshow("Image", image)
+# cv2.waitKey(0)
